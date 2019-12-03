7066b9486f0339f38427a3f819d9a42d93c819f5
Vaibhav Bhat <vaibhavbhat@Vaibhavs-MacBook-Pro.local>
2019-12-02 20:09:02 -0800

    asf

--- solver.py
+++ solver.py
@@ -6,6 +6,7 @@ import argparse
 import utils
 
 from student_utils import *
+from christofides import *
 """
 ======================================================================
   Complete the following function.
@@ -25,7 +26,11 @@ def solve(list_of_locations, list_of_homes, starting_car_location, adjacency_mat
         A dictionary mapping drop-off location to a list of homes of TAs that got off at that particular location
         NOTE: both outputs should be in terms of indices not the names of the locations themselves
     """
-    pass
+    # Rename vars for ease of use
+    locs  = list_of_locations
+    homes = list_of_homes
+    start = starting_car_location
+    edges = adjacency_matrix
 
 """
 ======================================================================
